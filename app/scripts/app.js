dbb1a744a52e663e295904c21e4e7ddca66966c8
Alexandre Masselot <alexandre.masselot@isb-sib.ch>
2014-12-05 11:05:27 +0100

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name msvizUiApp
+ * @description
+ * # msvizUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('msvizUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name msvizUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the msvizUiApp
+ */
+angular.module('msvizUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
