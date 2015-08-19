38765d87ae24ae0d6a5cc37c52eab66a49ec8571
rmylonas <roman.mylonas@isb-sib.ch>
2015-08-19 18:43:21 +0200

    show delta scores

--- app/main/compare/protein/alignment/psms.js
+++ app/main/compare/protein/alignment/psms.js
@@ -22,10 +22,8 @@ angular.module('matches-psms', ['protein-matches-pviz-view', 'psm-service', 'thi
 
     var link = function (scope, elm) {
       pviz.FeatureDisplayer.addMouseoverCallback(['psm'], function (ft) {
+        console.log(ft.data);
         scope.$broadcast('show-match', {type: 'psm', bean: ft.data});
-      });
-      pviz.FeatureDisplayer.addMouseoverCallback(['psmIsoModif'], function (ft) {
-        scope.$broadcast('show-match', {type: 'psmIsoModif', bean: ft.data});
       });
       pviz.FeatureDisplayer.addClickCallback(['psm'], function (ft) {
         addSelectedPSM(scope, ft.data);
