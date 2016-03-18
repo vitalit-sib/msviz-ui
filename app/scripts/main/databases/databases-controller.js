9d4b89b1a1a0cc0e733f207f41497208ca57cac1
Trinidad Martín <trinidad.martin@isb-sib.ch>
2016-03-18 16:01:06 +0100

    add database

--- app/scripts/main/databases/databases-controller.js
+++ app/scripts/main/databases/databases-controller.js
@@ -1,6 +1,66 @@
 'use strict';
 angular.module('databases-controller', ['thirdparties', 'environment','ngFileUpload','databases-services'])
 
+  .directive('ngFiles', ['$parse', function ($parse) {
+
+    function fn_link(scope, element, attrs) {
+      var onChange = $parse(attrs.ngFiles);
+      element.on('change', function (event) {
+        onChange(scope, { $files: event.target.files });
+      });
+    };
+
+    return {
+      link: fn_link
+    }
+  } ])
+  .controller('DatabasesCtrl', function ($scope,databasesService,$http,EnvConfig) {
+
+
+    databasesService.listFasta().then(function (data) {
+      $scope.databasesList = data;
+    });
+
+    //Delete selected searchIds
+    $scope.deleteEntry = function(sourceId){
+      databasesService.deleteFasta(sourceId).then(function () {
+        databasesService.listFasta().then(function (data) {
+          $scope.databasesList = data;
+        });
+      });
+    }
+
+
+    var filesData = new FormData();
+    $scope.getTheFiles = function ($files) {
+      filesData=$files[0]
+    };
+
+    // NOW UPLOAD THE FILES.
+    $scope.uploadFiles = function () {
+      var request = {
+        method: 'POST',
+        url: EnvConfig.backendUrl + '/sequences/' + filesData.name + '/fasta',
+        data: filesData,
+        headers: {
+          'Content-Type': undefined
+        }
+      };
+
+      // SEND THE FILES.
+      $http(request)
+        .success(function (d) {
+            databasesService.listFasta().then(function (data) {
+              console.log("listing")
+              $scope.databasesList = data;
+            });
+
+        })
+        .error(function () {
+        });
+    }
+  });
+/*
   .controller('DatabasesCtrl', function($scope, databasesService,Upload,$timeout,EnvConfig) {
     databasesService.listFasta().then(function (data) {
       $scope.databasesList = data;
@@ -34,5 +94,5 @@ angular.module('databases-controller', ['thirdparties', 'environment','ngFileUpl
           });
         }
     };
-  })
+  })*/
 
