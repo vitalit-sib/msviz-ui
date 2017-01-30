ae7478f613d92f604e0120812c501f2c3bd83655
rmylonas <roman.mylonas@isb-sib.ch>
2017-01-30 15:15:17 +0100

    corrected jshint

--- app/scripts/main/about-msviz.js
+++ app/scripts/main/about-msviz.js
@@ -3,11 +3,11 @@ angular.module('about-msviz', ['environment'])
 
   .controller('AboutCtrl', function(httpProxy, $scope) {
 
-    $scope.frontendVersion = "1.0.2";
+    $scope.frontendVersion = '1.0.2';
 
     httpProxy.get('/version').then(function(data){
       $scope.backendVersion = data;
     });
 
 
-  })
+  });
